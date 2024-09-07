f6086e123cfef57d33cdebdf9fbca9ce87adf93b
aqsaameen <aqsam9778@gmail.com>
2024-09-07 19:47:15 +0500

    update

---
+++ src/app/Profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return(
+    <div><img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/aqsa pic.png" alt="" width="384" height="512">
+        </img>
+    </div>
+  )
+}
+
+export default page
